269a5e4025ca79748527bb2ea9e4cfc5ad6c2985
chansub <chansub@users.noreply.github.com>
2022-12-15 02:54:06 +0000

    deploy: 29b6bdd4c503a6329ed2ad4d67640f7bad31d8ad

--- src/components/generic/DisplayRounds.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DisplayRounds = (props) => {
-    return (
-        <><span>{props.label ? props.label : ''}</span><span className="numbers rounds">{props.rounds}</span> {(props.rounds>1)?'rounds':'round'}</>
-    );
-};
-
-export default DisplayRounds;
--- src/components/generic/Textbox.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Textbox = ({...props}) => {
-  return <input type="text" {...props}>{props.children}</input>;
-};
-export default Textbox;
